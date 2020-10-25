1f8a1ac15172a6c3816fb6b5b881a5fcf257a8c9
Rafal <rafal.opial@coderslab.pl>
2020-10-25 17:09:08 +0100

    M6D4T4Z2 Factors - step 3.b implement some more functionality

--- src/main/java/pl/coderslab/unittestskrajeew05/model/Factors.java
+++ src/main/java/pl/coderslab/unittestskrajeew05/model/Factors.java
@@ -4,13 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Factors {
-    public static int[] generatePrimeFactors(int n){
-        if(n<1){
+    public static int[] generatePrimeFactors(int n) {
+        if (n < 1) {
             throw new IllegalArgumentException("Must be greater than 0");
         }
 
-        List<Integer> result = new ArrayList<>();
+        List<Integer> factors = new ArrayList<>();
+        factors.add(1);
 
-        return null;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                factors.add(i);
+            }
+        }
+
+        factors.add(n);
+
+        return factors.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
