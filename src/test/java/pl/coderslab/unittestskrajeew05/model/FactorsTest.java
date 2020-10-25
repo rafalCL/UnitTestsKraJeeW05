e13b8a903f21da486284a3eeb5756f698591baf3
Rafal <rafal.opial@coderslab.pl>
2020-10-25 17:15:10 +0100

    M6D4T4Z2 Factors - step 4 refactor and add more tests. mostly refactored tests not the code itself

--- src/test/java/pl/coderslab/unittestskrajeew05/model/FactorsTest.java
+++ src/test/java/pl/coderslab/unittestskrajeew05/model/FactorsTest.java
@@ -7,9 +7,33 @@ import static org.junit.Assert.*;
 public class FactorsTest {
 
     @Test
+    public void generatePrimeFactors2() {
+        final int[] expected = {1,2};
+        final int[] actual = Factors.generatePrimeFactors(2);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void generatePrimeFactors6() {
         final int[] expected = {1,2,3,6};
         final int[] actual = Factors.generatePrimeFactors(6);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void generatePrimeFactors9() {
+        final int[] expected = {1,3,9};
+        final int[] actual = Factors.generatePrimeFactors(9);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void generatePrimeFactors12() {
+        final int[] expected = {1,2,3,4,6,12};
+        final int[] actual = Factors.generatePrimeFactors(12);
 
         assertArrayEquals(expected, actual);
     }
@@ -22,13 +46,21 @@ public class FactorsTest {
         assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void generatePrimeFactors100() {
+        final int[] expected = {1,2,4,5,10,20,25,50,100};
+        final int[] actual = Factors.generatePrimeFactors(100);
+
+        assertArrayEquals(expected, actual);
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void generatePrimeFactors0() {
-        final int[] actual = Factors.generatePrimeFactors(0);
+        Factors.generatePrimeFactors(0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void generatePrimeFactorsNegativeNumber() {
-        final int[] actual = Factors.generatePrimeFactors(-5);
+        Factors.generatePrimeFactors(-5);
     }
 }
