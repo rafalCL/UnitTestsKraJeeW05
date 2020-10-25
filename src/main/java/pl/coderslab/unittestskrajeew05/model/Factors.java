d9b766869990fc9ded59817ae734543a22d8f462
Rafal <rafal.opial@coderslab.pl>
2020-10-25 16:50:29 +0100

    M6D4T4Z2 Factors - step 3.a implement some methods

--- src/main/java/pl/coderslab/unittestskrajeew05/model/Factors.java
+++ src/main/java/pl/coderslab/unittestskrajeew05/model/Factors.java
@@ -1,7 +1,16 @@
 package pl.coderslab.unittestskrajeew05.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factors {
     public static int[] generatePrimeFactors(int n){
+        if(n<1){
+            throw new IllegalArgumentException("Must be greater than 0");
+        }
+
+        List<Integer> result = new ArrayList<>();
+
         return null;
     }
 }
